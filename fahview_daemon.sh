0007c23dbe7b2527cbfc9ae427c537db3448f7b2
Colson Xu <colson.xu@gmail.com>
2020-04-30 14:44:04 -0700

    Added donor stats and removed 'trim()'

--- fahview_daemon.sh
+++ fahview_daemon.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # Data collecting Process for fahmon.sh
+# Dependencies: nvidia-smi, lm-sensors, curl, jq
 
-# Getting CPU Temperature from 'lm-sensor'
+#################### Temperature ####################
+# Getting CPU Temperature from 'lm-sensors'
 # Getting GPU Temperature and Power Draw from 'nvidia-smi'
-cpu_temp=`sensors | grep "Tdie" | cut -c16-19`
-t1050=`nvidia-smi | grep "P0" | cut -c9-10`
-t1080=`nvidia-smi | grep "P2" | cut -c9-10`
-p1050=`nvidia-smi | grep "P0" | cut -c22-24`
-p1080=`nvidia-smi | grep "P2" | cut -c21-31`
+# TO-DO: Parse data with something other than 'cut' to always get the acurate data
+
+# cpu_temp=`sensors | grep "Tdie" | cut -c16-19`
+# t1050=`nvidia-smi | grep "P0" | grep -o "[1-9][0-9]C"`
+# t1080=`nvidia-smi | grep "P2" | grep -o "[1-9][0-9]C"`
+# p1050=`nvidia-smi | grep "P0" | cut -c22-24`
+# p1080=`nvidia-smi | grep "P2" | cut -c21-31`
+
 
 
 echo  "+------------------------------------------+"
@@ -22,25 +27,37 @@ echo  "+------------------------------------------+"
 echo
 echo
 
-# Getting FAHClient Folding Progress from Log File
-FS0=`cat /var/lib/fahclient/log.txt | grep "FS00" | grep -Po "[1-9][0-9]?%|100%" | tail -n 1`
-FS1=`cat /var/lib/fahclient/log.txt | grep "FS01" | grep -Po "[1-9][0-9]?%|100%" | tail -n 1`
-FS2=`cat /var/lib/fahclient/log.txt | grep "FS02" | grep -Po "[1-9][0-9]?%|100%" | tail -n 1`
 
-# Remove "%" to Get Integer Value
-trim() {
-	FS=$1
-	if [ ${#FS} == 2 ]
-	then
-		echo $FS | cut -c1-1
-	elif [ ${#FS} == 3 ]
-	then
-		echo $FS | cut -c1-2
-	elif [ ${#FS} == 4 ]
-	then
-		echo $FS | cut -c1-3
-	fi
-}
+#################### Folding Identity ####################
+# TO-DO: Make donor_name dynamic
+donor_name="colsonxu"
+data="/tmp/fahview.tmp"
+# Getting data using Folding@home Web API
+# Stroing gathered data in /tmp
+# Potential issue with frequent API request <CAUTION!>
+curl -so $data "https://stats.foldingathome.org/api/donor/$donor_name"
+
+# Processing raw data
+global_rank="`jq .rank $data` / `jq .total_users $data`"
+total_credits=`jq .credit $data`
+current_team=`jq '.teams[-1] | .name' $data`
+credits_towards_current_team=`jq '.teams[-1] | .credit' $data`
+
+# Displaying processed data
+echo "Donor Name: $donor_name"
+echo "Total Credits: $total_credits"
+echo "Global Rank: $global_rank"
+echo "Current Team: $current_team"
+echo "Credits Towards Current Team: $credits_towards_current_team"
+echo
+
+
+#################### Folding Progress ####################
+# Getting FAHClient Folding Progress from Log File
+FS0=`cat /var/lib/fahclient/log.txt | grep "FS00" | grep -Po "[1-9][0-9]?%|100%" | tail -n 1 | grep -Po "[1-9][0-9]?|100"`
+FS1=`cat /var/lib/fahclient/log.txt | grep "FS01" | grep -Po "[1-9][0-9]?%|100%" | tail -n 1 | grep -Po "[1-9][0-9]?|100"`
+FS2=`cat /var/lib/fahclient/log.txt | grep "FS02" | grep -Po "[1-9][0-9]?%|100%" | tail -n 1 | grep -Po "[1-9][0-9]?|100"`
+
 
 prog() {
 	local w=50 p=$1;	shift
@@ -56,14 +73,11 @@ prog() {
 		FS=" GTX 1080 Ti"
 	fi
 	# Displaying Progress Bar
-	printf -v bars "%*s" "$(( $p*$w/100 ))" ""; bars=${bars// /#};
+	printf -v bars "%*s" "$(( $p*$w/100 ))" ""; bars=${bars// /=};
 	printf "$FS [%-*s]%3d%% %s \n" "$w" "$bars" "$p" "$*";
 }
 
 
-FS0=$(trim $FS0)
-FS1=$(trim $FS1)
-FS2=$(trim $FS2)
 prog $FS0
 prog $FS1
 prog $FS2
